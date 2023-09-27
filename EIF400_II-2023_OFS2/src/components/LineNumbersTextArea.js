cf2d67bbc07daf167cc1f54f49e5418b3aa4e82b
DanielaMadrigal <daniela.madrigal.morales@est.una.ac.cr>
2023-09-26 18:50:22 -0600

    CAMBIOS

--- EIF400_II-2023_OFS2/src/components/LineNumbersTextArea.js
+++ EIF400_II-2023_OFS2/src/components/LineNumbersTextArea.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
 
-function LineNumbersTextArea({ value, onChange }) {
-  const lines = value.split('\n');
-  
+const LineNumbersTextArea = ({ value })=> {  
   return (
-    <div className="line-numbers-text-area">
-      <div className="line-numbers">
-        {lines.map((_, index) => (
-          <div key={index} className="line-number">
-            {index + 1}
-          </div>
-        ))}
-      </div>
-      <textarea
-        value={value}
-        onChange={onChange}
-        className="text-area"
-      />
-    </div>
+    <textarea
+      className="custom-textarea-lines"
+      value={value}
+      style={{
+        zIndex: 1,
+      }}
+      readOnly
+    />
   );
-}
+};
 
 export default LineNumbersTextArea;
