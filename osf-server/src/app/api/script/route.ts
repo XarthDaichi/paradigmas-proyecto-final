c91197189c7e1efc3ab5aacf86ea473f06d49e6a
XarthDaichi <diegoqahh@gmail.com>
2023-11-01 15:47:02 -0600

    modificado script/route.ts

--- osf-server/src/app/api/script/route.ts
+++ osf-server/src/app/api/script/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server'
-import { loadScripts, write } from '@/lib/utils/fs'
 import { getScripts, insertScript } from '@/lib/mongo/scripts'
 import { Script } from '@/lib/types'
 
@@ -20,25 +19,19 @@ export async function OPTIONS(request: Request) {
 }
 
 export async function POST(req: Request) {
-    // let scripts = loadScripts()
     const { name, text }: Partial<Script> = await req.json()
 
     if (!name) return NextResponse.json({"message": "Missing script name"})
     if (!text) return NextResponse.json({"message": "Missing script text"})
 
-    // const _id = scripts.length + 1
-
     const newScript: Script = {name, text}
 
-    // scripts.push(newScript)
     const { insertedId, error }  = await insertScript(newScript);
-    // write('scripts.json', JSON.stringify({"scripts" : scripts}))
 
     return NextResponse.json({"message": `Script saved: ${insertedId}`})
 }
 
 export async function GET() {
-    // let scripts = loadScripts()
     const {scripts, error} = await getScripts()
     let names: Partial<Script>[] = []
 
