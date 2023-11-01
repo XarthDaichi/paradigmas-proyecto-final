f17fba2d657bbb2ca9c126d49e14804960675ef3
XarthDaichi <diegoqahh@gmail.com>
2023-11-01 10:21:28 -0600

    changes to test_1.js

--- test_1.mjs
+++ test_1.mjs
@@ -1,7 +1,5 @@
 import {Stream, Nats} from './combinadores.mjs';
 
-// const nats = iterable(0, (n) => n + 1);
-
 const nats = new Stream( Nats(0) );
 
 const even = nats.filter((n) => n % 2 === 0);
@@ -9,5 +7,5 @@ const even = nats.filter((n) => n % 2 === 0);
 const evenLessThanEleven = even.filter((n) => n < 11);
 
 for (const e of evenLessThanEleven.map((n) => console.log(n)).cut(10).toList()) {
-    console.log(e);
+    // console.log(e);
 }
